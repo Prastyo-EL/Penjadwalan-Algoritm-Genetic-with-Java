ab2a961ebfa24e932db3ba23f31f2e5373ee0b45
CI <nagaemasperkasa26@gmail.com>
2019-10-07 08:18:48 +0700

    add plugin

--- src/penjadwalan/koneksi.java
+++ src/penjadwalan/koneksi.java
@@ -21,7 +21,7 @@ public class koneksi {
         //Statement stm = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydbb","root","");
             //stm=con.createStatement();
             return con;
         }
